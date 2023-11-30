d51ab3a6a96a3394b4bd36e38fb2aad399dddd7e
KhalidDev <148249284+devAlq@users.noreply.github.com>
2023-11-30 14:29:43 -0800

    creating the first initializer of student.java

---
+++ student.java
@@ -0,0 +1,17 @@
+ package school;
+
+public class Student {
+    private String name;
+    private int studentId;
+    private int age;
+    private float grade;
+
+    public Student(String name, int studentId, int age, float grade){
+        this.name = name;
+        this.studentId = 
+    }
+    
+}
+public static void main(String[] args) {
+    
+}
